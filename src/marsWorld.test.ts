14bb8170f606310e80f5a1d5b27c56db32ec835a
Chris Giannakopoulos <christos@weaver.build>
2023-08-29 22:13:33 +0100

    Add more tests

--- src/marsWorld.test.ts
+++ src/marsWorld.test.ts
@@ -9,7 +9,14 @@ describe('Test mars world e2e', () => {
 
     it('should display (0, 4, W) LOST', () => {
         console.log = jest.fn();
-        marsWorld('4 8\n(2, 3, E) LFRFF\n(0, 2, N) FFLFRFF')
+        marsWorld('4 8\n(0, 2, N) FFLFRFF')
         expect(console.log).toHaveBeenCalledWith('(0, 4, W) LOST')
     })
+
+    it('should display (1, 0, S) LOST', () => {
+        console.log = jest.fn();
+        marsWorld('4 8\n(1, 0, S) FFRLF')
+        expect(console.log).toHaveBeenCalledWith('(1, 0, S) LOST')
+    })
+
 })
